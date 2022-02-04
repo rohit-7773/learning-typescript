5cd2375389bc1c7847fa31c9e37499dff7e0ba86
Rohit <rohit.gupta@neosoftmail.com>
2022-02-04 11:29:16 +0530

    simple class with public writeable property

---
+++ src/classes.ts
@@ -0,0 +1,13 @@
+class Point {
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+var point = new Point(1, 1);
+point.x = 2
+point.y = 2;
